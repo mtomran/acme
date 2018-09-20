d3ae93c153720f1d5112705d5de152157e7831a9
Masoud Omran <mo@t24defence.com>
2018-09-20 11:00:52 -0400

    added db connection and automatic restart

--- server/app.js
+++ server/app.js
@@ -2,10 +2,15 @@
 global.utils= require("./lib/utils");
 const express = require("express");
 
+// request body parser middleware
 const bodyParser= require("body-parser");
 
 // morgan request logger
 const morgan= require("morgan");
+
+const MongoClient = require("mongodb").MongoClient;
+
+
 
 const PORT = 8000;
 const HOST = "0.0.0.0";
@@ -23,6 +28,17 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1", require("./routes"));
 
-app.listen(PORT, HOST);
-console.log(`Running ${NAME} on http://${HOST}:${PORT}`);
+// Use connect method to connect to the server
+MongoClient.connect("mongodb://db:27017/acme", function (err, database) {
+	if(err) throw new Error(err);
+
+	console.log("Connected to DB successfully");	
+	global.db = database;
+
+	// running server after DB connection
+	console.log(`Running ${NAME} on http://${HOST}:${PORT}`);
+	app.listen(PORT, HOST);
+});
+
+
 module.exports = app;
